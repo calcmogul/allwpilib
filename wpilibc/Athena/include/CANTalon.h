9aeaed53dd1d98e5e9a63eaa4f994bbcf9449591
Peter Johnson <johnson.peter@gmail.com>
2016-05-20 16:14:17 -0700

    Fix up CANTalon.h comments.

--- wpilibc/Athena/include/CANTalon.h
+++ wpilibc/Athena/include/CANTalon.h
@@ -65,36 +65,27 @@ class CANTalon : public MotorSafety,
    * When in Motion Profile control mode, these constants are paseed
    * into set() to manipulate the motion profile executer.
    * When changing modes, be sure to read the value back using
-   * getMotionProfileStatus()
-   * to ensure changes in output take effect before performing buffering
-   * actions.
+   * getMotionProfileStatus() to ensure changes in output take effect before
+   * performing buffering actions.
    * Disable will signal Talon to put motor output into neutral drive.
    *   Talon will stop processing motion profile points.  This means the buffer
-   * is
-   *   effectively disconnected from the executer, allowing the robot to
-   * gracefully
-   *   clear and push new traj points.  isUnderrun will get cleared.
+   *   is effectively disconnected from the executer, allowing the robot to
+   *   gracefully clear and push new traj points.  isUnderrun will get cleared.
    *   The active trajectory is also cleared.
    * Enable will signal Talon to pop a trajectory point from it's buffer and
-   * process it.
-   *   If the active trajectory is empty, Talon will shift in the next point.
-   *   If the active traj is empty, and so is the buffer, the motor drive is
-   * neutral and
-   *   isUnderrun is set.  When active traj times out, and buffer has at least
-   * one point,
-   *   Talon shifts in next one, and isUnderrun is cleared.  When active traj
-   * times out,
-   *   and buffer is empty, Talon keeps processing active traj and sets
-   * IsUnderrun.
+   *   process it. If the active trajectory is empty, Talon will shift in the
+   *   next point. If the active traj is empty, and so is the buffer, the motor
+   *   drive is neutral and isUnderrun is set.  When active traj times out, and
+   *   buffer has at least one point, Talon shifts in next one, and isUnderrun
+   *   is cleared.  When active traj times out, and buffer is empty, Talon
+   *   keeps processing active traj and sets IsUnderrun.
    * Hold will signal Talon keep processing the active trajectory indefinitely.
    *   If the active traj is cleared, Talon will neutral motor drive.  Otherwise
-   *    Talon will keep processing the active traj but it will not shift in
-   *    points from the buffer.  This means the buffer is  effectively
-   * disconnected
-   *    from the executer, allowing the robot to gracefully clear and push
-   *    new traj points.
-   *    isUnderrun is set if active traj is empty, otherwise it is cleared.
-   *    isLast signal is also cleared.
+   *   Talon will keep processing the active traj but it will not shift in
+   *   points from the buffer.  This means the buffer is effectively
+   *   disconnected from the executer, allowing the robot to gracefully clear
+   *   and push new traj points. isUnderrun is set if active traj is empty,
+   *   otherwise it is cleared. isLast signal is also cleared.
    *
    * Typical workflow:
    *   set(Disable),
@@ -103,9 +94,9 @@ class CANTalon : public MotorSafety,
    *   set(Enable) when enough points have been pushed to ensure no underruns,
    *   wait for MP to finish or decide abort,
    *   If MP finished gracefully set(Hold) to hold position servo and disconnect
-   * buffer,
+   *   buffer,
    *   If MP is being aborted set(Disable) to neutral the motor and disconnect
-   * buffer,
+   *   buffer,
    *   Confirm mode takes effect,
    *   clear buffer and push buffer points, and rinse-repeat.
    */
@@ -121,11 +112,11 @@ class CANTalon : public MotorSafety,
   struct TrajectoryPoint {
     double position;  //!< The position to servo to.
     double velocity;  //!< The velocity to feed-forward.
-                      /**
-                       * Time in milliseconds to process this point.
-                       * Value should be between 1ms and 255ms.  If value is zero
-                       * then Talon will default to 1ms.  If value exceeds 255ms API will cap it.
-                       */
+    /**
+     * Time in milliseconds to process this point.
+     * Value should be between 1ms and 255ms.  If value is zero
+     * then Talon will default to 1ms.  If value exceeds 255ms API will cap it.
+     */
     unsigned int timeDurMs;
     /**
      * Which slot to get PIDF gains.
@@ -160,19 +151,19 @@ class CANTalon : public MotorSafety,
      */
     bool isLastPoint;
     /**
-      * Set to true to signal Talon to zero the selected sensor.
-      * When generating MPs, one simple method is to make the first target
+     * Set to true to signal Talon to zero the selected sensor.
+     * When generating MPs, one simple method is to make the first target
      * position zero,
-      * and the final target position the target distance from the current
+     * and the final target position the target distance from the current
      * position.
-      * Then when you fire the MP, the current position gets set to zero.
-      * If this is the intent, you can set zeroPos on the first trajectory
+     * Then when you fire the MP, the current position gets set to zero.
+     * If this is the intent, you can set zeroPos on the first trajectory
      * point.
-      *
-      * Otherwise you can leave this false for all points, and offset the
+     *
+     * Otherwise you can leave this false for all points, and offset the
      * positions
-      * of all trajectory points so they are correct.
-      */
+     * of all trajectory points so they are correct.
+     */
     bool zeroPos;
   };
   /**
@@ -184,10 +175,9 @@ class CANTalon : public MotorSafety,
      * The available empty slots in the trajectory buffer.
      *
      * The robot API holds a "top buffer" of trajectory points, so your
-     * applicaion
-     * can dump several points at once.  The API will then stream them into the
-     * Talon's
-     * low-level buffer, allowing the Talon to act on them.
+     * applicaion can dump several points at once.  The API will then stream
+     * them into the Talon's low-level buffer, allowing the Talon to act on
+     * them.
      */
     unsigned int topBufferRem;
     /**
@@ -208,8 +198,7 @@ class CANTalon : public MotorSafety,
     /**
      * This is set if Talon needs to shift a point from its buffer into
      * the active trajectory point however the buffer is empty. This gets
-     * cleared
-     * automatically when is resolved.
+     * cleared automatically when is resolved.
      */
     bool isUnderrun;
     /**
@@ -225,8 +214,8 @@ class CANTalon : public MotorSafety,
      * The current output mode of the motion profile executer (disabled,
      * enabled, or hold).
      * When changing the set() value in MP mode, it's important to check this
-     * signal to
-     * confirm the change takes effect before interacting with the top buffer.
+     * signal to confirm the change takes effect before interacting with the
+     * top buffer.
      */
     SetValueMotionProfile outputEnable;
   };
@@ -349,11 +338,11 @@ class CANTalon : public MotorSafety,
   /**
    * Enables Talon SRX to automatically zero the Sensor Position whenever an
    * edge is detected on the index signal.
+   *
    * @param enable      boolean input, pass true to enable feature or false to
-   * disable.
+   *                    disable.
    * @param risingEdge  boolean input, pass true to clear the position on rising
-   * edge,
-   *                    pass false to clear the position on falling edge.
+   *                    edge, pass false to clear the position on falling edge.
    */
   void EnableZeroSensorPositionOnIndex(bool enable, bool risingEdge);
   void ConfigSetParameter(uint32_t paramEnum, double value);
@@ -379,39 +368,35 @@ class CANTalon : public MotorSafety,
 
   /**
    * Calling application can opt to speed up the handshaking between the robot
-   * API and the Talon to increase the
-   * download rate of the Talon's Motion Profile.  Ideally the period should be
-   * no more than half the period
-   * of a trajectory point.
+   * API and the Talon to increase the download rate of the Talon's Motion
+   * Profile.  Ideally the period should be no more than half the period of a
+   * trajectory point.
    */
   void ChangeMotionControlFramePeriod(int periodMs);
 
   /**
    * Clear the buffered motion profile in both Talon RAM (bottom), and in the
-   * API (top).
-   * Be sure to check GetMotionProfileStatus() to know when the buffer is
-   * actually cleared.
+   * API (top). Be sure to check GetMotionProfileStatus() to know when the
+   * buffer is actually cleared.
    */
   void ClearMotionProfileTrajectories();
 
   /**
    * Retrieve just the buffer count for the api-level (top) buffer.
    * This routine performs no CAN or data structure lookups, so its fast and
-   * ideal
-   * if caller needs to quickly poll the progress of trajectory points being
-   * emptied
-   * into Talon's RAM. Otherwise just use GetMotionProfileStatus.
+   * ideal if caller needs to quickly poll the progress of trajectory points
+   * being emptied into Talon's RAM. Otherwise just use GetMotionProfileStatus.
    * @return number of trajectory points in the top buffer.
    */
   int GetMotionProfileTopLevelBufferCount();
 
   /**
    * Push another trajectory point into the top level buffer (which is emptied
-   * into
-   * the Talon's bottom buffer as room allows).
+   * into the Talon's bottom buffer as room allows).
+   *
    * @param trajPt the trajectory point to insert into buffer.
-   * @return true  if trajectory point push ok. CTR_BufferFull if buffer is full
-   * due to kMotionProfileTopBufferCapacity.
+   * @return true if trajectory point push ok. CTR_BufferFull if buffer is full
+   *         due to kMotionProfileTopBufferCapacity.
    */
   bool PushMotionProfileTrajectory(const TrajectoryPoint& trajPt);
 
@@ -422,13 +407,12 @@ class CANTalon : public MotorSafety,
 
   /**
    * This must be called periodically to funnel the trajectory points from the
-   * API's top level buffer to
-   * the Talon's bottom level buffer.  Recommendation is to call this twice as
-   * fast as the executation rate of the motion profile.
-   * So if MP is running with 20ms trajectory points, try calling this routine
-   * every 10ms.  All motion profile functions are thread-safe
-   * through the use of a mutex, so there is no harm in having the caller
-   * utilize threading.
+   * API's top level buffer to the Talon's bottom level buffer.  Recommendation
+   * is to call this twice as fast as the executation rate of the motion
+   * profile. So if MP is running with 20ms trajectory points, try calling this
+   * routine every 10ms.  All motion profile functions are thread-safe through
+   * the use of a mutex, so there is no harm in having the caller utilize
+   * threading.
    */
   void ProcessMotionProfileBuffer();
 
@@ -437,22 +421,19 @@ class CANTalon : public MotorSafety,
    * Since this all comes from one CAN frame, its ideal to have one routine to
    * retrieve the frame once and decode everything.
    * @param [out] motionProfileStatus contains all progress information on the
-   * currently running MP.
+   *                                  currently running MP.
    */
   void GetMotionProfileStatus(MotionProfileStatus& motionProfileStatus);
 
   /**
    * Clear the hasUnderrun flag in Talon's Motion Profile Executer when MPE is
-   * ready for another point,
-   * but the low level buffer is empty.
+   * ready for another point, but the low level buffer is empty.
    *
    * Once the Motion Profile Executer sets the hasUnderrun flag, it stays set
-   * until
-   * Robot Application clears it with this routine, which ensures Robot
-   * Application
-   * gets a chance to instrument or react.  Caller could also check the
-   * isUnderrun flag
-   * which automatically clears when fault condition is removed.
+   * until Robot Application clears it with this routine, which ensures Robot
+   * Application gets a chance to instrument or react.  Caller could also check
+   * the isUnderrun flag which automatically clears when fault condition is
+   * removed.
    */
   void ClearMotionProfileHasUnderrun();
 
@@ -498,39 +479,33 @@ class CANTalon : public MotorSafety,
   /**
    * Encoder CPR, counts per rotations, also called codes per revoluion.
    * Default value of zero means the API behaves as it did during the 2015
-   * season, each position
-   * unit is a single pulse and there are four pulses per count (4X).
-   * Caller can use ConfigEncoderCodesPerRev to set the quadrature encoder CPR.
+   * season, each position unit is a single pulse and there are four pulses per
+   * count (4X). Caller can use ConfigEncoderCodesPerRev to set the quadrature
+   * encoder CPR.
    */
   uint32_t m_codesPerRev = 0;
   /**
    * Number of turns per rotation.  For example, a 10-turn pot spins ten full
-   * rotations from
-   * a wiper voltage of zero to 3.3 volts.  Therefore knowing the
-   * number of turns a full voltage sweep represents is necessary for
-   * calculating rotations
-   * and velocity.
-   * A default value of zero means the API behaves as it did during the 2015
-   * season, there are 1024
-   * position units from zero to 3.3V.
+   * rotations from a wiper voltage of zero to 3.3 volts.  Therefore knowing
+   * the number of turns a full voltage sweep represents is necessary for
+   * calculating rotations and velocity. A default value of zero means the API
+   * behaves as it did during the 2015 season, there are 1024 position units
+   * from zero to 3.3V.
    */
   uint32_t m_numPotTurns = 0;
   /**
    * Although the Talon handles feedback selection, caching the feedback
-   * selection is helpful at the API level
-   * for scaling into rotations and RPM.
+   * selection is helpful at the API level for scaling into rotations and RPM.
    */
   FeedbackDevice m_feedbackDevice = QuadEncoder;
 
   static const unsigned int kDelayForSolicitedSignalsUs = 4000;
   /**
    * @param devToLookup FeedbackDevice to lookup the scalar for.  Because Talon
-   *                allows multiple sensors to be attached simultaneously,
-   * caller must
-   *                specify which sensor to lookup.
-   * @return        The number of native Talon units per rotation of the
-   * selected sensor.
-   *                Zero if the necessary sensor information is not available.
+   *                    allows multiple sensors to be attached simultaneously,
+   *                    caller must specify which sensor to lookup.
+   * @return The number of native Talon units per rotation of the selected
+   *         sensor. Zero if the necessary sensor information is not available.
    * @see ConfigEncoderCodesPerRev
    * @see ConfigPotentiometerTurns
    */
@@ -543,60 +518,55 @@ class CANTalon : public MotorSafety,
    */
   void ApplyControlMode(CANSpeedController::ControlMode mode);
   /**
-   * @param fullRotations   double precision value representing number of
-   * rotations of selected feedback sensor.
-   *                            If user has never called the config routine for
-   * the selected sensor, then the caller
-   *                            is likely passing rotations in engineering units
-   * already, in which case it is returned
-   *                            as is.
-   *                            @see ConfigPotentiometerTurns
-   *                            @see ConfigEncoderCodesPerRev
+   * @param fullRotations double precision value representing number of
+   *                      rotations of selected feedback sensor. If user has
+   *                      never called the config routine for the selected
+   *                      sensor, then the caller is likely passing rotations
+   *                      in engineering units already, in which case it is
+   *                      returned as is.
+   * @see ConfigPotentiometerTurns
+   * @see ConfigEncoderCodesPerRev
    * @return fullRotations in native engineering units of the Talon SRX
-   * firmware.
+   *         firmware.
    */
   int32_t ScaleRotationsToNativeUnits(FeedbackDevice devToLookup,
                                       double fullRotations) const;
   /**
-   * @param rpm     double precision value representing number of rotations per
-   * minute of selected feedback sensor.
-   *                            If user has never called the config routine for
-   * the selected sensor, then the caller
-   *                            is likely passing rotations in engineering units
-   * already, in which case it is returned
-   *                            as is.
-   *                            @see ConfigPotentiometerTurns
-   *                            @see ConfigEncoderCodesPerRev
+   * @param rpm double precision value representing number of rotations per
+   *            minute of selected feedback sensor. If user has never called
+   *            the config routine for the selected sensor, then the caller is
+   *            likely passing rotations in engineering units already, in which
+   *            case it is returned as is.
+   * @see ConfigPotentiometerTurns
+   * @see ConfigEncoderCodesPerRev
    * @return sensor velocity in native engineering units of the Talon SRX
-   * firmware.
+   *         firmware.
    */
   int32_t ScaleVelocityToNativeUnits(FeedbackDevice devToLookup,
                                      double rpm) const;
   /**
-   * @param nativePos     integral position of the feedback sensor in native
-   * Talon SRX units.
-   *                            If user has never called the config routine for
-   * the selected sensor, then the return
-   *                            will be in TALON SRX units as well to match the
-   * behavior in the 2015 season.
-   *                            @see ConfigPotentiometerTurns
-   *                            @see ConfigEncoderCodesPerRev
+   * @param nativePos integral position of the feedback sensor in native
+   *                  Talon SRX units. If user has never called the config
+   *                  routine for the selected sensor, then the return will be
+   *                  in TALON SRX units as well to match the behavior in the
+   *                  2015 season.
+   * @see ConfigPotentiometerTurns
+   * @see ConfigEncoderCodesPerRev
    * @return double precision number of rotations, unless config was never
-   * performed.
+   *         performed.
    */
   double ScaleNativeUnitsToRotations(FeedbackDevice devToLookup,
                                      int32_t nativePos) const;
   /**
-   * @param nativeVel     integral velocity of the feedback sensor in native
-   * Talon SRX units.
-   *                            If user has never called the config routine for
-   * the selected sensor, then the return
-   *                            will be in TALON SRX units as well to match the
-   * behavior in the 2015 season.
-   *                            @see ConfigPotentiometerTurns
-   *                            @see ConfigEncoderCodesPerRev
+   * @param nativeVel integral velocity of the feedback sensor in native
+   *                  Talon SRX units. If user has never called the config
+   *                  routine for the selected sensor, then the return will be
+   *                  in TALON SRX units as well to match the behavior in the
+   *                  2015 season.
+   * @see ConfigPotentiometerTurns
+   * @see ConfigEncoderCodesPerRev
    * @return double precision of sensor velocity in RPM, unless config was never
-   * performed.
+   *         performed.
    */
   double ScaleNativeUnitsToRpm(FeedbackDevice devToLookup,
                                int32_t nativeVel) const;
