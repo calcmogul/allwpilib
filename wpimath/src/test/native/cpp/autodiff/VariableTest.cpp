a0f89af6d1368bd567bd038dabcf68cb947eeec8
Tyler Veness <calcmogul@gmail.com>
2022-09-01 16:52:14 -0700

    Remove prints from Hessian tests

--- wpimath/src/test/native/cpp/autodiff/VariableTest.cpp
+++ wpimath/src/test/native/cpp/autodiff/VariableTest.cpp
@@ -2,7 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <fmt/core.h>
 #include <wpi/numbers>
 
 #include "frc/autodiff/TapeNode.h"
@@ -614,11 +613,9 @@ TEST(VariableTest, Hessian) {
                         (1.0 - 1.0 / (frc::autodiff::cos(x(i)) *
                                       frc::autodiff::cos(x(i))))
                             .Value(),
-                    H(i, j), 1e-14)
-            << fmt::format("  at ({}, {})\n", i, j);
+                    H(i, j), 1e-14);
       } else {
-        EXPECT_NEAR((g(j) / frc::autodiff::tan(x(i))).Value(), H(i, j), 1e-14)
-            << fmt::format("  at ({}, {})\n", i, j);
+        EXPECT_NEAR((g(j) / frc::autodiff::tan(x(i))).Value(), H(i, j), 1e-14);
       }
     }
   }
