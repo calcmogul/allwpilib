f7eb929fdbad6ed4945fe9b9abbf445e1acd432a
Tyler Veness <calcmogul@gmail.com>
2022-09-14 20:39:08 -0700

    Fix CurrentManager tests

--- wpimath/src/test/native/cpp/CurrentManagerTest.cpp
+++ wpimath/src/test/native/cpp/CurrentManagerTest.cpp
@@ -12,10 +12,10 @@ TEST(CurrentManagerTest, EnoughCurrent) {
 
   auto currents = manager.Calculate(wpi::array{25_A, 10_A, 5_A, 0_A});
 
-  EXPECT_DOUBLE_EQ(25.0, currents[0].value());
-  EXPECT_DOUBLE_EQ(10.0, currents[1].value());
-  EXPECT_DOUBLE_EQ(5.0, currents[2].value());
-  EXPECT_DOUBLE_EQ(0.0, currents[3].value());
+  EXPECT_NEAR(25.0, currents[0].value(), 1e-2);
+  EXPECT_NEAR(10.0, currents[1].value(), 1e-2);
+  EXPECT_NEAR(5.0, currents[2].value(), 1e-2);
+  EXPECT_NEAR(0.0, currents[3].value(), 1e-2);
 }
 
 TEST(CurrentManagerTest, NotEnoughCurrent) {
@@ -23,8 +23,38 @@ TEST(CurrentManagerTest, NotEnoughCurrent) {
 
   auto currents = manager.Calculate(wpi::array{30_A, 10_A, 5_A, 0_A});
 
-  EXPECT_DOUBLE_EQ(29.960317463044998, currents[0].value());
-  EXPECT_DOUBLE_EQ(9.007936577007202, currents[1].value());
-  EXPECT_DOUBLE_EQ(1.0317463470386752, currents[2].value());
-  EXPECT_DOUBLE_EQ(1.4545684970529923e-09, currents[3].value());
+  // Expected values are from the following CasADi program:
+  //
+  // #!/usr/bin/env python3
+  //
+  // import casadi as ca
+  // import numpy as np
+  //
+  // opti = ca.Opti()
+  // allocated_currents = opti.variable(4, 1)
+  //
+  // current_tolerances = np.array([[1.0], [5.0], [10.0], [5.0]])
+  // desired_currents = np.array([[30.0], [10.0], [5.0], [0.0]])
+  //
+  // current_sum = 0.0
+  // J = 0.0
+  // for i in range(4):
+  //     error = desired_currents[i, 0] - allocated_currents[i, 0]
+  //     J += error**2 / current_tolerances[i] ** 2
+  //
+  //     current_sum += allocated_currents[i, 0]
+  //
+  //     # Currents must be nonnegative
+  //     opti.subject_to(allocated_currents[i, 0] >= 0.0)
+  // opti.minimize(J)
+  //
+  // # Keep total current below maximum
+  // opti.subject_to(current_sum <= 40.0)
+  //
+  // opti.solver("ipopt")
+  // print(opti.solve().value(allocated_currents))
+  EXPECT_NEAR(29.960, currents[0].value(), 1e-2);
+  EXPECT_NEAR(9.00793, currents[1].value(), 1e-2);
+  EXPECT_NEAR(1.0317, currents[2].value(), 1e-2);
+  EXPECT_NEAR(1.454e-09, currents[3].value(), 1e-2);
 }
