90054a9b3da9127c9c0ec1b843215ddd0cea3e15
Tyler Veness <calcmogul@gmail.com>
2025-07-02 12:45:37 -0700

    [upstream_utils] Upgrade Sleipnir

--- upstream_utils/sleipnir.py
+++ upstream_utils/sleipnir.py
@@ -52,8 +52,8 @@ using small_vector = wpi::SmallVector<T>;
 def main():
     name = "sleipnir"
     url = "https://github.com/SleipnirGroup/Sleipnir"
-    # main on 2025-05-18
-    tag = "2cc18ff6d25ee0a9bd0f9993a0a41a61a28bda3e"
+    # main on 2025-07-10
+    tag = "6c2b9302b4aff3d270e1defa196a4899a9b94b5d"
 
     sleipnir = Lib(name, url, tag, copy_upstream_src)
     sleipnir.main()
