0cfebcea72ae95e4824ffea0186f25a7acf0d4d5
Tyler Veness <calcmogul@gmail.com>
2025-09-04 19:20:30 -0700

    [wpimath] Call SQP solver directly for Ellipse2d::Nearest()

--- wpimath/src/main/native/cpp/geometry/Ellipse2d.cpp
+++ wpimath/src/main/native/cpp/geometry/Ellipse2d.cpp
@@ -4,7 +4,9 @@
 
 #include "wpi/math/geometry/Ellipse2d.hpp"
 
-#include <sleipnir/optimization/problem.hpp>
+#include <cmath>
+
+#include <sleipnir/optimization/solver/sqp.hpp>
 
 using namespace wpi::math;
 
@@ -20,29 +22,60 @@ Translation2d Ellipse2d::Nearest(const Translation2d& point) const {
 
   // Find nearest point
   {
-    slp::Problem<double> problem;
+    // min (x − x_0)² + (y − y_0)²
+    // x,y
+    // s.t. (x − x_c)²/a² + (y − y_c)²/b² = 1
+    const double x_0 = rotPoint.X().value();
+    const double y_0 = rotPoint.Y().value();
+    const double x_c = m_center.X().value();
+    const double y_c = m_center.Y().value();
+    const double a2 = m_xSemiAxis.value() * m_xSemiAxis.value();
+    const double b2 = m_ySemiAxis.value() * m_ySemiAxis.value();
 
-    // Point on ellipse
-    auto x = problem.decision_variable();
-    x.set_value(rotPoint.X().value());
-    auto y = problem.decision_variable();
-    y.set_value(rotPoint.Y().value());
+    Eigen::VectorXd x{{x_0}, {y_0}};
+    slp::sqp(
+        slp::SQPMatrixCallbacks<double>{
+            [&](const Eigen::VectorXd& x) -> double {
+              // f(x) = (x[0] − x_0)² + (x[1] − y_0)²
+              return std::pow(x[0] - x_0, 2) + std::pow(x[1] - y_0, 2);
+            },
+            [&](const Eigen::VectorXd& x) -> Eigen::SparseVector<double> {
+              // ∇f(x) = [2(x[0] − x_0)]
+              //         [2(x[1] − y_0)]
+              return Eigen::Vector2d{{2.0 * (x[0] - x_0)}, {2.0 * (x[1] - y_0)}}
+                  .sparseView();
+            },
+            [&](const Eigen::VectorXd& x,
+                const Eigen::VectorXd& y) -> Eigen::SparseMatrix<double> {
+              // L(x, y) = f(x) − yᵀcₑ(x)
+              //
+              // ∇ₓₓ²L(x, y) = [2 − 2b²y     0    ]
+              //               [   0      2 − 2a²y]
+              return Eigen::SparseMatrix<double>{
+                  Eigen::DiagonalMatrix<double, 2>{2.0 - 2.0 * b2 * y[0],
+                                                   2.0 - 2.0 * a2 * y[0]}};
+            },
+            [&](const Eigen::VectorXd& x) -> Eigen::VectorXd {
+              // (x − x_c)²/a² + (y − y_c)²/b² = 1
+              // b²(x − x_c)² + a²(y − y_c)² = a²b²
+              // b²(x − x_c)² + a²(y − y_c)² − a²b² = 0
+              //
+              // cₑ(x) = b²(x − x_c)² + a²(y − y_c)² − a²b²
+              return Eigen::Vector<double, 1>{{b2 * std::pow(x[0] - x_c, 2) +
+                                               a2 * std::pow(x[1] - y_c, 2) -
+                                               a2 * b2}}
+                  .sparseView();
+            },
+            [&](const Eigen::VectorXd& x) -> Eigen::SparseMatrix<double> {
+              // Aₑ(x) = [2b²(x[0] − x_c)  2a²(x[1] − y_c)]
+              return Eigen::RowVector2d{
+                  {2.0 * b2 * (x[0] - x_c), 2.0 * a2 * (x[1] - y_c)}}
+                  .sparseView();
+            }},
+        {}, {}, x);
 
-    problem.minimize(slp::pow(x - rotPoint.X().value(), 2) +
-                     slp::pow(y - rotPoint.Y().value(), 2));
-
-    // (x − x_c)²/a² + (y − y_c)²/b² = 1
-    // b²(x − x_c)² + a²(y − y_c)² = a²b²
-    double a2 = m_xSemiAxis.value() * m_xSemiAxis.value();
-    double b2 = m_ySemiAxis.value() * m_ySemiAxis.value();
-    problem.subject_to(b2 * slp::pow(x - m_center.X().value(), 2) +
-                           a2 * slp::pow(y - m_center.Y().value(), 2) ==
-                       a2 * b2);
-
-    problem.solve();
-
-    rotPoint = wpi::math::Translation2d{wpi::units::meter_t{x.value()},
-                                        wpi::units::meter_t{y.value()}};
+    rotPoint =
+        wpi::math::Translation2d{units::meter_t{x[0]}, units::meter_t{x[1]}};
   }
 
   // Undo rotation
